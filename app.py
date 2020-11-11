ead8e03063ea8ac51efdefa30ababaef578a9ffa
Raunak Chowdhuri <raunakc@mit.edu>
2020-11-11 11:29:56 -0800

    Condensed displayed information into graph
    Also reintegrated raunaks' changes which seemed to be removed before

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import geocoder
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from plotly.colors import label_rgb as rgb
 import os
 
 st.beta_set_page_config()
@@ -64,42 +65,54 @@ if excel_file is not None:
                          title='Class Locations', size_max=25)
     # fig.show()
     '## Analysis'
-    st.plotly_chart(fig, use_container_width=True)
+    with st.beta_expander('General Statistics'):
+        st.success(f'There have been **{len(locations)}** classes taught with these filter options.')
+        st.success(f'The classes were taught in **{len(set(location_names))}** different cities.')
+        st.plotly_chart(fig, use_container_width=True)
+        st.success(f'These classes reached **{len(df)}** students.')
+
+        heritage_counts = df["History & Heritage Positive Motivators?"].str.lower().value_counts()
+        yes = heritage_counts.get('yes', 1)
+        no = heritage_counts.get('no', 0)
+        st.success(f'**{100 * yes / (yes + no):.2f}%** of the '
+                   f'{(yes + no)} students surveyed, said heritage/history '
+                   f'are positive motivators for health.')
 
     with st.beta_expander('Improvements'):
-        topics = ['Cooking Frequency', 'Herbs and Spices', 'Greens', 'Whole Grains', 'Beans', 'Tubers', 'Vegetables', 'Fruits',
+        topics = ['Cooking Frequency', 'Herbs and Spices', 'Greens', 'Whole Grains', 'Beans', 'Tubers', 'Vegetables',
+                  'Fruits',
                   'Vegetarian-Based Meals', 'Exercise']
-        for i in range(0, 10):
-
+        percentages = []
+        for i in range(len(topics)):
             # Create header names
             pre_string = "Pre"
             pre_name = "Pre - Num"
             post_name = "Post Num"
-            if i != 0:
+
+            if i != 0:  # artifact of how spreadsheet is formatted
                 pre_name += ("." + str(i))
                 post_name += ("." + str(i))
                 pre_string += ("." + str(i))
 
             pre_post = df[[pre_name, post_name, pre_string]]
             pre_post["Difference"] = pre_post[post_name] - pre_post[pre_name]
-            pre_post.dropna(inplace = True)  # drops the blank lines (they didn't answer)
+            pre_post.dropna(inplace=True)  # drops the blank lines (they didn't answer)
 
-            # Loop through and find the total number of people, number that increased, and number that stayed the same
-            total_num = 0
-            increase_num = 0
-            same_num = 0
-            for k in range(len(pre_post)):
-                if pre_post["Difference"].iloc[k] > 0:
-                    increase_num += 1
-                elif pre_post["Difference"].iloc[k] == 0:
-                    same_num += 1
-                total_num += 1
+            total_num = len(pre_post)
+            increase_num = len(pre_post[pre_post['Difference'] > 0])
+            same_num = len(pre_post[pre_post['Difference'] == 0])
+
             percent_increase = round((float(increase_num) / total_num) * 100, 2)
             percent_same = round((float(same_num) / total_num) * 100, 2)
-            st.success("Percent that increased {} is: {}% ({} people out of {} total)".format(topics[i], percent_increase, increase_num, total_num))
 
-            # more data: Can add if we want
-            # st.warning("Percent that remained the same for {} is: {}% ({} people out of {} total)".format(topics[i], percent_same, same_num, total_num))
-            # st.success("The mean increase is {}".format(pre_post["Difference"].mean()))
+            percentages.append([percent_increase, 'Increased', topics[i]])
+            percentages.append([percent_same, 'No Change', topics[i]])
+            percentages.append([100 - percent_increase - percent_same, 'Decreased', topics[i]])
+
+        percentage_df = pd.DataFrame(percentages, columns=['% of People Surveyed', 'Change', 'Category'])
+        st.plotly_chart(px.bar(percentage_df, x='Category', y='% of People Surveyed', color='Change',
+                               color_discrete_map={'Increased': rgb((166, 216, 84)),
+                                                   'No Change': rgb((255, 217, 47)),
+                                                   'Decreased': rgb((252, 141, 98))}))
     if st.checkbox('Show Raw Data'):
         df
